1b1ad174117a72d584409c24a5a2e7853a657037
Sandro Figo <sandro.figo@gmx.at>
2023-08-15 11:37:13 +0200

    Add asset post processor

---
+++ src/Assets/SimpleCodeGenerator/Editor/AssetPostprocessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace SimpleCodeGenerator.Editor
+{
+    public class AssetPostprocessor : UnityEditor.AssetPostprocessor
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            var allChangedAssets = importedAssets.Concat(deletedAssets).Concat(movedAssets).Concat(movedAssets);
+
+            if (allChangedAssets.Any(s =>
+                    s.Contains(".cs") ||
+                    s.Contains(".json")))
+                CodeGenerator.GenerateAll();
+        }
+    }
+}
---
+++ src/Assets/SimpleCodeGenerator/Editor/CodeGenerator.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+
+namespace SimpleCodeGenerator.Editor
+{
+    public static class CodeGenerator
+    {
+        [MenuItem("Code Generation/Generate All", false, 0)]
+        public static void GenerateAll()
+        {
+            
+        }
+    }
+}
