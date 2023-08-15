55af9fe822e78d25f14cd4a17f70227a4cc4e301
Sandro Figo <sandro.figo@gmx.at>
2023-08-15 11:46:53 +0200

    Call methods marked with attribute

--- src/Assets/SimpleCodeGenerator/Editor/CodeGenerator.cs
+++ src/Assets/SimpleCodeGenerator/Editor/CodeGenerator.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 
 namespace SimpleCodeGenerator.Editor
@@ -7,7 +10,18 @@ namespace SimpleCodeGenerator.Editor
         [MenuItem("Code Generation/Generate All", false, 0)]
         public static void GenerateAll()
         {
-            
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            var methods = assemblies
+                .SelectMany(a => a.GetTypes())
+                .SelectMany(t => t.GetMethods())
+                .Where(m => Attribute.GetCustomAttribute(m, typeof(CodeGenerationMethod)) != null)
+                .ToArray();
+
+            foreach (MethodInfo methodInfo in methods)
+            {
+                methodInfo.Invoke(null, null);
+            }
         }
     }
 }
