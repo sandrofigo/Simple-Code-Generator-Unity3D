df2b5ac8131f41b46840f0cdf6c7a96f940136fb
Sandro Figo <sandro.figo@gmx.at>
2023-08-16 18:14:44 +0200

    Add GitHub workflow for tests

--- build/Build.cs
+++ build/Build.cs
@@ -2,17 +2,25 @@ using System;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.CI.GitHubActions.Configuration;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
+using Octokit;
 using Serilog;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 
+[GitHubActions("test",
+    GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = new[] { "main" },
+    InvokedTargets = new[] { nameof(Test) }
+)]
 class Build : NukeBuild
 {
     public static int Main() => Execute<Build>(x => x.Test);
@@ -24,12 +32,14 @@ class Build : NukeBuild
     readonly Solution UnitTestProject;
 
     Target Clean => _ => _
-        .Before(Restore)
         .Executes(() =>
         {
+            DotNetTasks.DotNetClean(s => s
+                .SetProject(UnitTestProject));
         });
 
     Target Restore => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
         {
             DotNetTasks.DotNetRestore(settings => settings
@@ -43,15 +53,22 @@ class Build : NukeBuild
         {
             DotNetTasks.DotNetBuild(settings => settings
                 .SetProjectFile(UnitTestProject)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
             );
         });
 
     Target Test => _ => _
         .DependsOn(Compile)
+        .Produces(RootDirectory / "tests" / "TestResults" / "*.trx")
         .Executes(() =>
         {
             DotNetTasks.DotNetTest(settings => settings
                 .SetProjectFile(UnitTestProject)
+                .SetConfiguration(Configuration)
+                .SetLoggers("trx;logfilename=test-results.trx")
+                .EnableNoRestore()
+                .EnableNoBuild()
             );
         });
 }
