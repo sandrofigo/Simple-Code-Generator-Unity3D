80edb2316fcafc14a53162ab6dee3d3ac641166e
Sandro Figo <sandro.figo@gmx.at>
2023-08-19 14:31:24 +0200

    Add more tests

---
+++ tests/AbsolutePathTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using SimpleCodeGenerator.Editor;
+
+namespace tests;
+
+public class AbsolutePathTests
+{
+    [Fact]
+    public void Constructor_AbsolutePathFromRelativePath_AbsolutePathIsCorrect()
+    {
+        var result = (AbsolutePath)".";
+
+        result.ToString().Should().Be(Path.GetFullPath(".").Replace('\\', '/'));
+    }
+
+    [Theory]
+    [InlineData("test", "/test")]
+    [InlineData("/test", "/test")]
+    [InlineData("//test", "/test")]
+    [InlineData("///test", "/test")]
+    [InlineData("/test/123", "/test/123")]
+    [InlineData("/test/123/", "/test/123")]
+    public void DivisionOperator_AbsolutePathIsJoinedWithString_CreatesCorrectAbsolutePath(string input, string expected)
+    {
+        AbsolutePath result = (AbsolutePath)"." / input;
+
+        result.ToString().Should().Be(Path.GetFullPath(".").Replace('\\', '/') + expected);
+    }
+}
---
+++ tests/CodeGeneratorTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using SimpleCodeGenerator.Editor;
+
+namespace tests;
+
+[UsesVerify]
+public class CodeGeneratorTests
+{
+    [Fact]
+    public Task Render_RenderBuiltInStringDictionaryTemplate_TemplateIsRenderedCorrectly()
+    {
+        var values = new StringDictionaryItem[]
+        {
+            new("Apple", "Fruit", "An apple is a fruit."),
+            new("Banana", "Fruit", "A banana is a fruit."),
+            new("Potato", "Vegetable", "A potato is a vegetable.")
+        };
+
+        CodeGenerator.GenerateStringDictionary(values, "TestNamespace", "TestClass", "result.txt");
+
+        string result = File.ReadAllText("result.txt");
+
+        var settings = new VerifySettings();
+        settings.UseDirectory("Verify");
+        return Verify(result, settings);
+    }
+
+    [Fact]
+    public Task Render_RenderBuiltInEnumTemplate_TemplateIsRenderedCorrectly()
+    {
+        string[] values =
+        {
+            "Hello",
+            "World",
+            "1Apple",
+            "2 Bananas"
+        };
+
+        CodeGenerator.GenerateEnum(values, "TestNamespace", "TestEnum", "result.txt");
+
+        string result = File.ReadAllText("result.txt");
+
+        var settings = new VerifySettings();
+        settings.UseDirectory("Verify");
+        return Verify(result, settings);
+    }
+
+    [Theory]
+    [InlineData("test", "test")]
+    [InlineData("test123", "test123")]
+    [InlineData("1test", "_1test")]
+    [InlineData("1 test", "_1test")]
+    [InlineData("hello world", "helloworld")]
+    [InlineData(" hello world", "helloworld")]
+    [InlineData(" hello   world", "helloworld")]
+    [InlineData(" hello 1 world", "hello1world")]
+    [InlineData(" ", "")]
+    [InlineData("   ", "")]
+    [InlineData("/test", "test")]
+    [InlineData("test%$&§!?`+-#", "test")]
+    public void SanitizeStringForVariableName_SanitizeStrings_StringAreSanitizedCorrectly(string input, string expected)
+    {
+        string result = CodeGenerator.SanitizeStringForVariableName(input);
+
+        result.Should().Be(expected);
+    }
+}
